e440df34a785441be417b2b3f5f28f944e1b19a6
JNeto <joaomendes15@gmail.com>
2023-03-19 19:04:43 +0000

    ex5 done

--- Books_Register/src/Book.java
+++ Books_Register/src/Book.java
@@ -1,11 +1,11 @@
 public class Book {
 
-    String name = "default";
-    String author = "default";
-    String publisher = "default";
-    String city = "default";
-    int publishingYear = 0;
-    int editionNumber = 0;
+    private String name = "default";
+    private String author = "default";
+    private String publisher = "default";
+    private String city = "default";
+    private int publishingYear = 0;
+    private int editionNumber = 0;
 
     public Book() {}
     public Book(String name, String author, String publisher, String city, int publishingYear, int editionNumber) {
@@ -17,14 +17,27 @@ public class Book {
         this.editionNumber = editionNumber;
     }
 
+    public void printBookInfo() {
+        System.out.println(this);
+    }
+
     @Override
     public String toString() {
         return String.format("Book \nname: %s\nauthor: %s\npublisher: %s\ncity: %s\npublishing year: %d\nedition: %d",
                               name, author, publisher, city, publishingYear, editionNumber);
     }
 
-    public void printBookInfo() {
-        System.out.println(this);
-    }
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+    public String getAuthor() {return author;}
+    public void setAuthor(String author) {this.author = author;}
+    public String getPublisher() {return publisher;}
+    public void setPublisher(String publisher) {this.publisher = publisher;}
+    public String getCity() {return city;}
+    public void setCity(String city) {this.city = city;}
+    public int getPublishingYear() {return publishingYear;}
+    public void setPublishingYear(int publishingYear) {this.publishingYear = publishingYear;}
+    public int getEditionNumber() {return editionNumber;}
+    public void setEditionNumber(int editionNumber) {this.editionNumber = editionNumber;}
 
 }
